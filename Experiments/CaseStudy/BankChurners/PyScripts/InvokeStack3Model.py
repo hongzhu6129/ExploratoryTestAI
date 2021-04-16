83d66f0b882239e2c86e271686636715d3e39cdd
Hong Zhu <hzhu@brookes.ac.uk>
2021-04-16 12:25:33 +0100

    Add files via upload

---
+++ Experiments/CaseStudy/BankChurners/PyScripts/InvokeStack3Model.py
@@ -0,0 +1,24 @@
+# Call SK learned Model
+import sys
+import pickle
+
+#Set model's file name
+fileName = "C:\Morphy\BankChurners\models\Stack3_model.sav"
+
+#Load model
+model = pickle.load(open(fileName, 'rb'))
+
+#Predict on input parameters
+while True:
+    #Read parameters
+    #print("Please enter 10 parameters:")
+    iParam = sys.stdin.readline().rstrip()
+    if iParam == "":
+        break  #end of prediction
+    params = [float(s) for s in iParam.split(',')]
+    #predict
+    result = model.predict([params])
+    print(result[0])
+    sys.stdout.flush()
+print("Python script stopped: ")
+sys.stdout.flush()
